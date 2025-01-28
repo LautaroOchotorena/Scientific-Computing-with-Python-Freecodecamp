3ecb8e29d69ee3240bd3cedff015fe78776f5105
Lautaro <125595446+LautaroOchotorena@users.noreply.github.com>
2025-01-28 20:24:33 -0300

    Update shape_calculator.py

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -1,4 +1,3 @@
-#Rectangle Class
 class Rectangle:
 	def __init__(self, wid, heig):	#Constructor
 		self.width = wid
@@ -13,7 +12,7 @@ class Rectangle:
 		return(2*self.height+2*self.width)
 	def get_diagonal(self):
 		return((self.width ** 2 + self.height ** 2) ** .5)
-	def get_picture(self):  #it prints the rectangle
+	def get_picture(self):
 		if(self.height<=50 and self.width<=50):
 			text=''
 			for k in range(1,self.height+1):
@@ -21,7 +20,7 @@ class Rectangle:
 			return(text)
 		else:
 			return("Too big for picture.")
-	def get_amount_inside(self,shape):  #how many amount of shapes fit into the rectangle
+	def get_amount_inside(self,shape):
 		heig=self.height
 		wid=self.width
 		count=0
@@ -40,8 +39,6 @@ class Rectangle:
 		return(count)
 	def __str__(self):
 		return('Rectangle(width='+str(self.width)+', height='+str(self.height)+')')
-
-#Square Class
 class Square(Rectangle):
 	def __init__(self, side):
 		self.height=side
@@ -49,5 +46,26 @@ class Square(Rectangle):
 	def set_side(self,side):
 		self.height=side
 		self.width=side
+	def set_width(self,wid):
+		self.set_side(wid)
+	def set_height(self,heig):
+		self.set_side(heig)
 	def __str__(self):
 		return('Square(side='+str(self.width)+')')
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
